1f8b1a72abc93c93523edc37f71ffbaace0ff865
Kwangsu-CHOI <wisecks@gmail.com>
2024-07-25 00:18:08 +1000

    fifth3

--- firebase.ts
+++ firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCU-H5-UyGdIiSF8vGE5T7Tf9vADQIcjCY",
-	authDomain: "notion-clone-92001.firebaseapp.com",
-	projectId: "notion-clone-92001",
-	storageBucket: "notion-clone-92001.appspot.com",
-	messagingSenderId: "369540728586",
-	appId: "1:369540728586:web:138660a8c544edfc1b9cf3",
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
